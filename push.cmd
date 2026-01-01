30245af61afd2992c434cde06c2974c666f7f34b
Untrammeled <211125@ms.fkyc.edu.hk>
2026-01-01 17:22:42 +0800

    Update

--- push.cmd
+++ push.cmd
@@ -1,6 +1,8 @@
-git init
+# 1. 首先添加所有文件
 git add .
-git commit -m "初始提交：HKDSE笔记共享中心"
-git branch -M main
-git remote add origin git@github.com:findbaicha/HKDSEr_Notehub.git
-git push -u origin main
+
+# 2. 提交更改
+git commit -m "Update"
+
+# 3. 推送到GitHub仓库
+git push origin master
